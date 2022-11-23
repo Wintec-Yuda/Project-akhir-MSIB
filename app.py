b25f4e59ec65236e1b7b253f2eb1a482dd411eb4
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2022-11-23 21:04:14 +0700

    Added Item and Content Based Recommendation

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
+import mysql.connector as db
 import re
 
 app = Flask(__name__, static_url_path='/static')
+mydb = db.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="novel_recommender"
+)
 
 
 @app.route('/')
@@ -14,16 +23,17 @@ def home():
 def get_title():
     if request.method == 'POST':
         title = request.form['book']
-        mask = books['Book-Title'].str.contains(title)
-        myresult = books[mask]
-        myresult = myresult.iloc[:99]
+        mycursor = mydb.cursor()
+        mycursor.execute(f"SELECT title FROM books WHERE books.title LIKE '%{title}%' LIMIT 20")
+        myresult = mycursor.fetchall()
+
         titleList = []
-        for book in myresult["Book-Title"]:
-            titleList.append(book)
+        for book in myresult:
+            titleList.append(book[0])
 
         return jsonify({
             "data": titleList
-        })
+            })
 
 
 @app.route('/get_recommend', methods=['GET', 'POST'])
@@ -36,14 +46,13 @@ def get_recommend():
         print(mode)
 
         if mode == '1':
-            print("Content-Based")
-        #     novels = content_based(title)
+            result = content_based(title)
         elif mode == '2':
-            print("Item-Based")
-        #     novels = item_based(title)
+            result = item_based(title)
         
         return jsonify({
-            "data": "GOGOGO"
+            "book-status": result["status"],
+            "data": result["data"]
         })
 
 
@@ -52,10 +61,10 @@ def get_popular():
     popular = popular_books(df)
     popList = []
     
-    for i in range(len(popular["Book-Title"].tolist())):
-        title = popular["Book-Title"].tolist()[i]
-        url = df.loc[df["Book-Title"]==popular["Book-Title"].tolist()[i],"Image-URL-L"][:1].values[0]
-        rating = round(df[df["Book-Title"]==popular["Book-Title"].tolist()[i]]["Book-Rating"].mean(),2)
+    for i in range(len(popular["title"].tolist())):
+        title = popular["title"].tolist()[i]
+        url = df.loc[df["title"]==popular["title"].tolist()[i],"image_l"][:1].values[0]
+        rating = round(df[df["title"]==popular["title"].tolist()[i]]["Book-Rating"].mean(),2)
         popList.append([title, rating, url])
 
     return jsonify({
@@ -65,15 +74,15 @@ def get_popular():
 
 def popular_books(df, n=4):
     rating_count = df.groupby(
-        "Book-Title").count()["Book-Rating"].reset_index()
+        "title").count()["Book-Rating"].reset_index()
     rating_count.rename(columns={"Book-Rating": "NumberOfVotes"}, inplace=True)
 
     rating_average = df.groupby(
-        "Book-Title")["Book-Rating"].mean().reset_index()
+        "title")["Book-Rating"].mean().reset_index()
     rating_average.rename(
         columns={"Book-Rating": "AverageRatings"}, inplace=True)
 
-    popularBooks = rating_count.merge(rating_average, on="Book-Title")
+    popularBooks = rating_count.merge(rating_average, on="title")
 
     def weighted_rate(x):
         v = x["NumberOfVotes"]
@@ -90,19 +99,94 @@ def popular_books(df, n=4):
     return popularBooks.iloc[:n]
 
 
+def content_based(bookTitle):
+    bookTitle = str(bookTitle)
+
+    if bookTitle in df["title"].values:
+        rating_count = pd.DataFrame(df["title"].value_counts())
+        rare_books = rating_count[rating_count["title"] <= 200].index
+        common_books = df[~df["title"].isin(rare_books)]
+
+        if bookTitle in rare_books:
+            most_common = pd.Series(
+                common_books["title"].unique()).sample(3).values
+            return {"status": "rare", "data": most_common}
+        else:
+            common_books = common_books.drop_duplicates(subset=["title"])
+            common_books.reset_index(inplace=True)
+            common_books["index"] = [i for i in range(common_books.shape[0])]
+            targets = ["title", "author", "publisher"]
+            common_books["all_features"] = [" ".join(
+                common_books[targets].iloc[i, ].values) for i in range(common_books[targets].shape[0])]
+            vectorizer = CountVectorizer()
+            common_booksVector = vectorizer.fit_transform(
+                common_books["all_features"])
+            similarity = cosine_similarity(common_booksVector)
+            index = common_books[common_books["title"]
+                                 == bookTitle]["index"].values[0]
+            similar_books = list(enumerate(similarity[index]))
+            similar_booksSorted = sorted(
+                similar_books, key=lambda x: x[1], reverse=True)[1:6]
+            books = []
+            for i in range(len(similar_booksSorted)):
+                books.append(common_books[common_books["index"] ==
+                             similar_booksSorted[i][0]]["title"].item())
+
+            return {"status": "normal", "data": books}
+
+    else:
+        return {"status": "na", "data": None}
+
+
+def item_based(bookTitle):
+    bookTitle = str(bookTitle)
+
+    if bookTitle in df["title"].values:
+        rating_count = pd.DataFrame(df["title"].value_counts())
+        rare_books = rating_count[rating_count["title"] <= 200].index
+        common_books = df[~df["title"].isin(rare_books)]
+
+        if bookTitle in rare_books:
+            most_common = pd.Series(
+                common_books["title"].unique()).sample(3).values
+            return {"status": "rare", "data": most_common}
+        else:
+            common_books_pivot = common_books.pivot_table(
+                index=["User-ID"], columns=["title"], values="Book-Rating")
+            title = common_books_pivot[bookTitle]
+            recommendation_df = pd.DataFrame(common_books_pivot.corrwith(
+                title).sort_values(ascending=False)).reset_index(drop=False)
+
+            if bookTitle in [title for title in recommendation_df["title"]]:
+                recommendation_df = recommendation_df.drop(
+                    recommendation_df[recommendation_df["title"] == bookTitle].index[0])
+            less_rating = []
+            for i in recommendation_df["title"]:
+                if df[df["title"] == i]["Book-Rating"].mean() < 5:
+                    less_rating.append(i)
+            if recommendation_df.shape[0] - len(less_rating) > 5:
+                recommendation_df = recommendation_df[~recommendation_df["title"].isin(
+                    less_rating)]
+
+            recommendation_df = recommendation_df[0:5]
+            recommendation_df.columns = ["title", "Correlation"]
+            return {"status": "normal", "data": recommendation_df["title"].tolist()}
+    else:
+        return {"status": "na", "data": None}
+
 if __name__ == '__main__':
-    books = pd.read_csv("model/data/Books.csv", delimiter=":::")
+    books = pd.read_sql("SELECT * FROM books", mydb)
+    books.rename(columns= {'isbn':'ISBN'}, inplace=True)
     ratings = pd.read_csv("model/data/Ratings.csv")
-    users = pd.read_csv("model/data/Users.csv")
 
     books_data = books.merge(ratings, on="ISBN")
     df = books_data.copy()
     df.dropna(inplace=True)
     df.reset_index(drop=True, inplace=True)
-    df.drop(columns=["Year-Of-Publication",
-                     "Image-URL-S", "Image-URL-M"], axis=1, inplace=True)
+    df.drop(columns=["publication_year",
+                     "image_s", "image_m"], axis=1, inplace=True)
     df.drop(index=df[df["Book-Rating"] == 0].index, inplace=True)
-    df["Book-Title"] = df["Book-Title"].apply(
+    df["title"] = df["title"].apply(
         lambda x: re.sub("[\W_]+", " ", x).strip())
 
     app.run(host="localhost", port=5000, debug=True)
