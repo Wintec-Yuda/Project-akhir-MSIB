9c73c86b62c519388e6c4a60381552c76f519a3b
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2022-11-23 15:53:19 +0700

    Added Popular Books Section

--- app.py
+++ app.py
@@ -1,33 +1,99 @@
 from flask import Flask, render_template, request, jsonify, Response
-import mysql.connector
+import pandas as pd
+import re
 
 app = Flask(__name__, static_url_path='/static')
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="novel_recommender"
-)
-        
+
+
 @app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/get_title', methods = ['GET', 'POST'])
+
+@app.route('/get_title', methods=['GET', 'POST'])
 def get_title():
     if request.method == 'POST':
         title = request.form['book']
-        mycursor = mydb.cursor()
-        mycursor.execute(f"SELECT title FROM books WHERE books.title LIKE '%{title}%' LIMIT 20")
-        myresult = mycursor.fetchall()
-        
+        mask = books['Book-Title'].str.contains(title)
+        myresult = books[mask]
+        myresult = myresult.iloc[:99]
         titleList = []
-        for book in myresult:
-            titleList.append(book[0])
-        
+        for book in myresult["Book-Title"]:
+            titleList.append(book)
+
         return jsonify({
             "data": titleList
-            })
+        })
+
+
+@app.route('/get_recommend', methods=['GET', 'POST'])
+def get_recommend():
+    if request.method == 'POST':
+        title = request.form['book']
+
+        print(title)
+
+        return jsonify({
+            "data": "Data received"
+        })
+
+
+@app.route('/get_popular')
+def get_popular():
+    popular = popular_books(df)
+    popList = []
     
+    for i in range(len(popular["Book-Title"].tolist())):
+        title = popular["Book-Title"].tolist()[i]
+        url = df.loc[df["Book-Title"]==popular["Book-Title"].tolist()[i],"Image-URL-L"][:1].values[0]
+        rating = round(df[df["Book-Title"]==popular["Book-Title"].tolist()[i]]["Book-Rating"].mean(),2)
+        popList.append([title, rating, url])
+
+    return jsonify({
+        "data": popList
+    })
+
+
+def popular_books(df, n=4):
+    rating_count = df.groupby(
+        "Book-Title").count()["Book-Rating"].reset_index()
+    rating_count.rename(columns={"Book-Rating": "NumberOfVotes"}, inplace=True)
+
+    rating_average = df.groupby(
+        "Book-Title")["Book-Rating"].mean().reset_index()
+    rating_average.rename(
+        columns={"Book-Rating": "AverageRatings"}, inplace=True)
+
+    popularBooks = rating_count.merge(rating_average, on="Book-Title")
+
+    def weighted_rate(x):
+        v = x["NumberOfVotes"]
+        R = x["AverageRatings"]
+
+        return ((v*R) + (m*C)) / (v+m)
+
+    C = popularBooks["AverageRatings"].mean()
+    m = popularBooks["NumberOfVotes"].quantile(0.90)
+
+    popularBooks = popularBooks[popularBooks["NumberOfVotes"] >= 250]
+    popularBooks["Popularity"] = popularBooks.apply(weighted_rate, axis=1)
+    popularBooks = popularBooks.sort_values(by="Popularity", ascending=False)
+    return popularBooks.iloc[:n]
+
+
 if __name__ == '__main__':
+    books = pd.read_csv("model/data/Books.csv", delimiter=":::")
+    ratings = pd.read_csv("model/data/Ratings.csv")
+    users = pd.read_csv("model/data/Users.csv")
+
+    books_data = books.merge(ratings, on="ISBN")
+    df = books_data.copy()
+    df.dropna(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    df.drop(columns=["Year-Of-Publication",
+                     "Image-URL-S", "Image-URL-M"], axis=1, inplace=True)
+    df.drop(index=df[df["Book-Rating"] == 0].index, inplace=True)
+    df["Book-Title"] = df["Book-Title"].apply(
+        lambda x: re.sub("[\W_]+", " ", x).strip())
+
     app.run(host="localhost", port=5000, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -2,16 +2,41 @@ $(document).ready(function () {
     var title = "";
     var titleList = [];
 
+    $.get("/get_popular", function(data, status){
+        $.each(data["data"], function (key, value) {
+            $("#populer-card").append(`
+            <div class="col-lg-3 p-1">
+                <div class="card-inner p-3 d-flex flex-column align-items-center h-100"> 
+                    <img class="rounded img-fluid" src="`+value[2]+`" style="height: 210px !important;">
+                    <div class="align-self-center h-100">
+                        <div class="text-center mg-text"> 
+                            <span class="mg-text">
+                                `+value[0]+`
+                            </span> 
+                        </div>
+                        <div class="text-center"> 
+                            <span id="rating">
+                                Rating : `+value[1]+`
+                            </span> 
+                        </div> 
+                    </div>
+                </div> 
+            </div>
+            `)
+        });
+        $("#populer-holder").css("display", "block");
+    });
+
     jQuery.ui.autocomplete.prototype._resizeMenu = function () {
         var ul = this.menu.element;
         ul.outerWidth(this.element.outerWidth());
     }
 
     $("#input-title").on("keyup", function () {
-        title = $(this).val();
+        title = $(this).val().toLowerCase();
         titleList = []
 
-        if (title.length == 4) {
+        if (title.length%3 == 0){
             $.post("/get_title", {
                 book: title
             }, function (data, status) {
@@ -19,22 +44,31 @@ $(document).ready(function () {
                     titleList.push(value)
                 });
                 $("#input-title").autocomplete({
-                    source: titleList
+                    source: titleList,
+                    position: { my : "right top+15", at: "right bottom" }
                 });
             });
         }
+    });
 
+    $("#input-title").on("autocompleteselect", function (event, ui) {
+        title = ui.item.value
     });
 
     $("#search-btn").click(function (e) {
         e.preventDefault();
-
+        $("#input-title").val("")
         if (title != "") {
+            $.post("/get_recommend", {
+                book: title
+            }, function (data, status) {
+                console.log("Data: " + data["data"] + "\nStatus: " + status);
+            });
 
-
-
+            $('html, body').animate({
+                scrollTop: $("#recommend-holder").offset().top
+            }, 2000);
         }
-
     });
 
 });
